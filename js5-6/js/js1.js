473c154effb3a648ae1dc36c14ca155db4c7d1c5
Oksana <ksusha_tsov@mail.ru>
2016-04-01 18:57:14 +0300

    commit 1/4

--- js5-6/js/js1.js
+++ js5-6/js/js1.js
@@ -1,62 +1,43 @@
 
-var hour = document.getElementById("hour").innerHTML;
-var min = document.getElementById("min").innerHTML;
-var sec = document.getElementById("sec").innerHTML;
-var ms = document.getElementById("ms").innerHTML;
-
 function timer() {
-if (ms>=999) {
-    ms = 0;
-    sec++;
-  }
-else if (sec==59) {
-  sec = 0;
-  min++;
-}
-else if (min==59) {
-  min = 0;
-  hour++;
-}
-else if (hour==23) {
-  sec = 0;
-  min = 0;
-  hour = 0;
-}
-else {
-  ms = (4 +  Number(ms));
-};
-// console.log(String(sec).length);
-if (String(sec).length < 2) {sec = '0' + sec};
-if (String(min).length < 2) {min = '0' + min};
-if (String(hour).length < 2) {hour = '0' + hour};
-document.getElementById("hour").innerHTML = hour;
-document.getElementById("min").innerHTML = min;
-document.getElementById("sec").innerHTML = sec;
-document.getElementById("ms").innerHTML = ms;
-// console.log(hour+':'+min+':'+sec+ms)
-};
+  var thisTime = new Date();
+  var diff = thisTime - beginTime;
+  var hour = (Math.floor(diff/1000/60/60)) + Number(hourFirst);
+  var min = (Math.floor((diff-hour*60*60*1000)/1000/60)) + Number(minFirst);
+  var sec = (Math.floor(((diff-hour*60*60*1000)-min*60*1000)/1000)) + Number(secFirst);
+  var ms = (diff + Number(msFirst)) % 1000;
 
+  if (String(sec).length < 2) {sec = '0' + sec};
+  if (String(min).length < 2) {min = '0' + min};
+  if (String(hour).length < 2) {hour = '0' + hour};
+  document.getElementById("hour").innerHTML = hour;
+  document.getElementById("min").innerHTML = min;
+  document.getElementById("sec").innerHTML = sec;
+  document.getElementById("ms").innerHTML = ms;
+  // console.log(hour+':'+min+':'+sec+':'+ms)
+};
 function handler1() {
   var b1Text = document.getElementById("b1").innerHTML;
+  hourFirst = document.getElementById("hour").innerHTML;
+  minFirst = document.getElementById("min").innerHTML;
+  secFirst = document.getElementById("sec").innerHTML;
+  msFirst = document.getElementById("ms").innerHTML;
+  beginTime = new Date();
   if (b1Text == "Start") {
     s = setInterval(timer, 1);
     document.getElementById("b1").innerHTML = "Pause";
-  }
+  };
   if (b1Text == "Pause") {
     clearInterval(s);
     document.getElementById("b1").innerHTML = "Start";
-  }
+  };
 };
 function handler2() {
   document.getElementById("hour").innerHTML = "00";
   document.getElementById("min").innerHTML = "00";
   document.getElementById("sec").innerHTML = "00";
-  document.getElementById("ms").innerHTML = "0000";
-  hour = 0;
-  min = 0;
-  sec = 0;
-  ms = 0;
-}
+  document.getElementById("ms").innerHTML = "000";
+};
 
 var b1 = document.getElementById("b1");
 b1.addEventListener("click", handler1);
@@ -67,12 +48,10 @@ var p = document.getElementsByClassName('point');
 function point () {
   if (p[0].style.visibility == 'visible') {
   p[0].style.visibility = 'hidden';
-  document.getElementsByClassName('point')[0].innerHTML = " ";
-  document.getElementsByClassName('point')[1].innerHTML = " ";
+  p[1].style.visibility = 'hidden';
   } else {
   p[0].style.visibility = 'visible';
-  document.getElementsByClassName('point')[0].innerHTML = ":";
-  document.getElementsByClassName('point')[1].innerHTML = ":";
-  }
-}
+  p[1].style.visibility = 'visible';
+  };
+};
 setInterval(point, 1000);
