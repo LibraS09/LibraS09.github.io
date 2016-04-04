fae72467f89f27b684a93f9fc46c0547d65ecf7d
Oksana <ksusha_tsov@mail.ru>
2016-04-04 19:56:13 +0300

    commit 4/4

--- js5-6/js/js1.js
+++ js5-6/js/js1.js
@@ -1,11 +1,11 @@
 
 function timer() {
   var thisTime = new Date();
-  var diff = thisTime - beginTime;
-  var hour = (Math.floor(diff/1000/60/60)) + Number(hourFirst);
-  var min = (Math.floor((diff-hour*60*60*1000)/1000/60)) + Number(minFirst);
-  var sec = (Math.floor(((diff-hour*60*60*1000)-min*60*1000)/1000)) + Number(secFirst);
-  var ms = (diff + Number(msFirst)) % 1000;
+  var diff = (thisTime - beginTime) + timeFirst;
+  var hour = Math.floor(diff/1000/60/60);
+  var min = Math.floor((diff-hour*60*60*1000)/1000/60);
+  var sec = Math.floor(((diff-hour*60*60*1000)-min*60*1000)/1000);
+  var ms = diff % 1000;
 
   if (String(sec).length < 2) {sec = '0' + sec};
   if (String(min).length < 2) {min = '0' + min};
@@ -17,11 +17,12 @@ function timer() {
   // console.log(hour+':'+min+':'+sec+':'+ms)
 };
 function handler1() {
-  var b1Text = document.getElementById("b1").innerHTML;
-  hourFirst = document.getElementById("hour").innerHTML;
-  minFirst = document.getElementById("min").innerHTML;
-  secFirst = document.getElementById("sec").innerHTML;
-  msFirst = document.getElementById("ms").innerHTML;
+  b1Text = document.getElementById("b1").innerHTML;
+  var hourFirst =  Number(document.getElementById("hour").innerHTML);
+  var minFirst =  Number(document.getElementById("min").innerHTML);
+  var secFirst =  Number(document.getElementById("sec").innerHTML);
+  var msFirst =  Number(document.getElementById("ms").innerHTML);
+  timeFirst = msFirst+secFirst*1000+minFirst*60*1000+hourFirst*60*60*1000;
   beginTime = new Date();
   if (b1Text == "Start") {
     s = setInterval(timer, 1);
@@ -33,10 +34,12 @@ function handler1() {
   };
 };
 function handler2() {
+  clearInterval(s);
   document.getElementById("hour").innerHTML = "00";
   document.getElementById("min").innerHTML = "00";
   document.getElementById("sec").innerHTML = "00";
   document.getElementById("ms").innerHTML = "000";
+  document.getElementById("b1").innerHTML = "Start";
 };
 
 var b1 = document.getElementById("b1");
