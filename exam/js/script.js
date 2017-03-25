bfa2b79e6c44707aafe698485e0455dc4d487b5a
Oksana <ksusha_tsov@mail.ru>
2017-03-25 18:45:14 +0200

    25.03

--- exam/js/script.js
+++ exam/js/script.js
@@ -18,17 +18,31 @@ $(function(){
   search(val);
 
   function search(v) {
-  var url= "http://api.pixplorer.co.uk/image?word="+v+"&amount=7";
+  var url= "https://pixabay.com/api/?key=4919792-453f009144b7806cb72243b60&q="+v+"&image_type=photo";
   $.getJSON(url, function (data){
-    var results = data.images;
+    var results = data.hits;
     console.log (data);
     for (var i=0; i<results.length; i++) {
       var r = results[i];
       var s = "#img"+i;
-      $(s).css({'background': 'url('+r.imageurl+')', 'background-size':'cover'});
-      $(s).html(r.word);
+      $(s).css({'background': 'url('+r.webformatURL+')', 'background-size':'cover'});
+      $(s).html(r.tags);
     }
   });
   };
+  //
+  // function search(v) {
+  // var url= "http://api.pixplorer.co.uk/image?word="+v+"&amount=7";
+  // $.getJSON(url, function (data){
+  //   var results = data.images;
+  //   console.log (data);
+  //   for (var i=0; i<results.length; i++) {
+  //     var r = results[i];
+  //     var s = "#img"+i;
+  //     $(s).css({'background': 'url('+r.imageurl+')', 'background-size':'cover'});
+  //     $(s).html(r.word);
+  //   }
+  // });
+  // };
 
 });
